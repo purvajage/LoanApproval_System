98c0609dd96537f0830ab5b7b67c9a3d75f7a760
purva jage <purvajage44@gmail.com>
2025-07-31 22:52:13 +0530

    commit

--- ai-model/train_model.py
+++ ai-model/train_model.py
@@ -57,8 +57,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Train
 model.fit(X_train, y_train)
-
-# Evaluate
 preds = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, preds))
 
