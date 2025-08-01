0584f52dba40155939fda2cfc4609d2c535e2bc6
purva jage <purvajage44@gmail.com>
2025-08-01 10:19:10 +0530

    commit

--- ai-model/train_model.py
+++ ai-model/train_model.py
@@ -54,11 +54,9 @@ model = Pipeline([
 
 # Train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 # Train
 model.fit(X_train, y_train)
 preds = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, preds))
-
 # Saving model
 joblib.dump(model, "model.pkl")
