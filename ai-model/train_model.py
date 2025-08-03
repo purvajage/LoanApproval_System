476ae4b4b3e0075e5575d5ee606ae15fee701365
purva jage <purvajage44@gmail.com>
2025-08-03 18:05:59 +0530

    commit

--- ai-model/train_model.py
+++ ai-model/train_model.py
@@ -52,11 +52,10 @@ model = Pipeline([
     ("classifier", LogisticRegression(max_iter=300))
 ])
 
-# Train/test split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# Train
+
 model.fit(X_train, y_train)
 preds = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, preds))
-# Saving model
 joblib.dump(model, "model.pkl")
